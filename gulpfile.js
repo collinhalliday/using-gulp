7eb0790b397da5463a647825e625d9c105293814
Collin Halliday <collinhalliday@gmail.com>
2018-06-23 22:51:13 -0600

    Moved all source files to a src folder. Changed project such that server runs from dist folder. Also combined methods dealing with concatenation and minification of css and js files respectively to allow mapping to following the entire process. This completes the general project specifications.

--- gulpfile.js
+++ gulpfile.js
@@ -9,68 +9,93 @@ const gulp = require('gulp'),
     rename = require('gulp-rename'),
     csso = require('gulp-csso'),
     imagemin = require('gulp-imagemin'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    htmlreplace = require('gulp-html-replace');
 
-gulp.task("concatScripts", function() {
-    return gulp.src([
-        'js/jquery-3.3.1.js',
-        'js/circle/autogrow.js',
-        'js/circle/circle.js'
+const options = {
+  src: 'src',
+  dist: 'dist'
+};
+
+gulp.task('copy', function() {
+  gulp.src(options.src + '/icons/**')
+  .pipe(gulp.dest(options.dist + '/icons'))
+});
+
+gulp.task("scripts", function() {
+    gulp.src([
+        options.src + '/js/jquery-3.3.1.js',
+        options.src + '/js/circle/autogrow.js',
+        options.src + '/js/circle/circle.js'
         ])
     .pipe(maps.init())
     .pipe(concat('global.js'))
-    .pipe(maps.write('./'))
-    .pipe(gulp.dest('js'));
-});
-
-gulp.task("scripts", ["concatScripts"], function() {
-  return gulp.src("js/global.js")
+    .pipe(gulp.dest(options.dist + '/scripts'))
     .pipe(uglify())
     .pipe(rename('all.min.js'))
-    .pipe(gulp.dest('dist/scripts'));
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest(options.dist + '/scripts'));
 });
 
-gulp.task('compileSass', function() {
-  return gulp.src("sass/global.scss")
-      .pipe(maps.init())
-      .pipe(sass())
-      .pipe(maps.write('./'))
-      .pipe(gulp.dest('css'));
-});
-
-gulp.task("styles", ["compileSass"], function() {
-  return gulp.src("css/global.css")
+gulp.task("styles", function() {
+  return gulp.src(options.src + '/sass/global.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(gulp.dest(options.dist + '/styles'))
     .pipe(csso())
     .pipe(rename('all.min.css'))
-    .pipe(gulp.dest('dist/styles'));
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest(options.dist + '/styles'));
 });
 
-gulp.task('images', function() {
-  gulp.src('images/*')
+gulp.task("images", function() {
+  gulp.src(options.src + '/images/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/content'))
+        .pipe(gulp.dest(options.dist + '/content'))
 });
 
 //Deletes files created by gulp
-gulp.task('clean', function() {
-  // del(['dist', 'css/application.css**', 'js/app*.js*']);
-  del(['css', 'dist/**', '!dist', 'js/global*.js**']);
-  //del(['css', 'dist', 'js/global*.js**']);
+gulp.task("clean", function() {
+  //del([options.src + '/css', options.dist + '/**', '!' + options.dist, options.src + '/js/global*.js**']);
+  del([options.dist + '/**', '!' + options.dist]);
+});
+
+gulp.task("html", function() {
+  gulp.src(options.src + '/index.html')
+    .pipe(htmlreplace({
+        'css': 'styles/all.min.css',
+        'js': 'scripts/all.min.js',
+        img: {
+          src: 'content/m-spore.png',
+          tpl: '<img src="%s" />'
+        },
+        img2: {
+          src: 'content/1.jpg',
+          tpl: '<img src="%s" />'
+        },
+        img3: {
+          src: 'content/3.jpg',
+          tpl: '<img src="%s" />'
+        },
+        img4: {
+          src: 'content/2.jpg',
+          tpl: '<img src="%s" />'
+        }
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task("build", ['clean'], function() {
   return gulp.start(['scripts', 'styles', 'images']);
-  // return gulp.src(['css/application.css', 'js/app.min.js', 'index.html', 'img/**', 'fonts/**'], { base: './'})
-  //   .pipe(gulp.dest('dist'));
 });
 
-gulp.task('connect', function() {
-  connect.server({
-    root: '.',
+gulp.task("connect", ['html'], function() {
+  return connect.server({
+    root: './' + options.dist,
     livereload: true
   })
 });
 
-gulp.task("default", ["build"], function() {
+gulp.task("default", ['build'], function() {
   return gulp.start('connect');
 });
